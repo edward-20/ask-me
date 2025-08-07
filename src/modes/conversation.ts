85ea28d03a86c4fd51f531e3633217002a33e1f1
Edward Lim <ed.lim199@gmail.com>
2025-08-07 14:02:54 +1000

    conversation works for initial questions

--- src/modes/conversation.ts
+++ src/modes/conversation.ts
@@ -6,6 +6,7 @@ import gsap from "gsap";
 import type { Mode } from "../mode";
 import { typeWord } from "../typing";
 import type { EventListener } from "../eventListener";
+import { div } from "three/tsl";
 
 export class ConversationMode implements Mode {
   camera: THREE.Camera;
@@ -16,7 +17,6 @@ export class ConversationMode implements Mode {
   }
 
   init() {
-    console.log("hello")
     gsap.to(this.camera.position, {
       duration: 2,
       x: 0,
@@ -27,10 +27,46 @@ export class ConversationMode implements Mode {
         this.camera.lookAt(0,0,0);
       }
     })
+    // use babel here?
     const divElement = document.createElement('div');
     divElement.className = "info";
     document.body.appendChild(divElement);
-    typeWord("Hi, what would you like to know about me?", document.getElementsByClassName("info")[0]);
+
+    const span = document.createElement("span");
+    span.className = "message";
+
+    const exitButton = document.createElement("button")
+    exitButton.textContent = "x";
+    exitButton.className = "exit"
+    
+
+    divElement.appendChild(exitButton);
+    divElement.appendChild(span);
+    typeWord("Hi, what would you like to know about me?", document.getElementsByClassName("message")[0]);
+
+    fetch("http://localhost:5000/")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json(); // Parse JSON body
+    })
+    .then((data : { question: string, answer: string }[]) => {
+      console.log(data);
+      const questionBoxes = data.map(qa => {
+        const div = document.createElement("div");
+        div.style.border = "black solid 1px";
+        div.textContent = qa.question;
+        return div;
+      })
+      questionBoxes.forEach((questionBox) => { divElement.append(questionBox); return; });
+      // divElement.append(question);
+      // divElement.style.border = "black";
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+
     this.eventListeners.forEach((eventListener) => {
       document.addEventListener(eventListener.event, eventListener.listener)
     })
